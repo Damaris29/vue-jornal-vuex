b774f66e464a3bab0a69b3a0e413d827373f1e64
Damaris Chinlli <damaris.chinlli@ixion.com.ec>
2022-02-15 08:05:33 -0500

    primer commit

---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <button class="btn btn-danger mx-2">
+            Borrar
+            <i class="fa fa-trash-alt"></i>
+        </button>
+
+        <button class="btn btn-primary">
+            Subir foto
+            <i class="fa fa-upload"></i>
+        </button>
+    </div>
+    <hr>
+    <div class="d-flex flex-column px-3 h-75">
+        <textarea
+            
+            placeholder="¿Qué sucedió hoy?"
+        ></textarea>
+    </div>
+
+        <img 
+            src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg" 
+            alt="entry-picture"
+            class="img-thumbnail">
+     <Fab 
+        icon="fa-save"
+    /> 
+
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+ 
+
+export default {
+    
+    components: {
+        Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+textarea {
+    font-size: 20px;
+    border: none;
+    height: 100%;
+
+    &:focus {
+        outline: none;
+    }
+}
+
+img {
+    width: 200px;
+    position: fixed;
+    bottom: 150px;
+    right: 20px;
+    box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+}
+
+</style>
